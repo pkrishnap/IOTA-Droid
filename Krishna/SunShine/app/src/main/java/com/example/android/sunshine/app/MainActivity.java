6e1a7089f0cff6b9715565bb8092d091d5ab508f
pkrishnap <pkrishnap@gmail.com>
2014-10-15 16:01:23 +0530

    Settings
    Fixed issue with settings

--- Krishna/SunShine/app/src/main/java/com/example/android/sunshine/app/MainActivity.java
+++ Krishna/SunShine/app/src/main/java/com/example/android/sunshine/app/MainActivity.java
@@ -50,7 +50,7 @@ public class MainActivity extends ActionBarActivity {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
         if (id == R.id.action_settings) {
-            startActivity(new Intent(this,SettingsActivity.class));
+            startActivity(new Intent(this, SettingsActivity.class));
             return true;
         }
         return super.onOptionsItemSelected(item);
